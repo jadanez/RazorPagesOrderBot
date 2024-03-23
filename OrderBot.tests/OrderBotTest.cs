eaaf71fa08c3380a82d9de06a4d06e9ca2a62b80
jadanez <122333839+jadanez@users.noreply.github.com>
2024-03-23 11:23:17 -0400

    .unit tests

--- OrderBot.tests/OrderBotTest.cs
+++ OrderBot.tests/OrderBotTest.cs
@@ -23,18 +23,17 @@ namespace OrderBot.tests
 
             }
         }
-        [Fact]
-        public void Test1()
-        {
 
-        }
+
+
         [Fact]
-        public void TestWelcome()
+        public void TestWelcome() //state welcoming
         {
             Session oSession = new Session("12345");
             String sInput = oSession.OnMessage("Hello")[0];
-            Assert.True(sInput.Contains("Welcome to Cafe Bot"));
+            Assert.True(sInput.Equals("Hey there! This is Fit All Day - your all day round fitness assistant."));
         }
+        
         [Fact]
         public void TestWelcomPerformance()
         {
@@ -46,40 +45,75 @@ namespace OrderBot.tests
             System.Diagnostics.Debug.WriteLine("Elapsed Time: " + nElapsed);
             Assert.True(nElapsed < 10000);
         }
+
+
         [Fact]
-        public void TestShawarama()
-        {
-            Session oSession = new Session("12345");
-            String sInput = oSession.OnMessage("hello")[0];
-            Assert.True(sInput.ToLower().Contains("shawarama"));
-        }
-        [Fact]
-        public void TestSize()
-        {
-            Session oSession = new Session("12345");
-            String sInput = oSession.OnMessage("hello")[1];
-            Assert.True(sInput.ToLower().Contains("size"));
-        }
-        [Fact]
-        public void TestLarge()
+        public void TestExercise()
         {
             Session oSession = new Session("12345");
             oSession.OnMessage("hello");
-            String sInput = oSession.OnMessage("large")[0];
-            Assert.True(sInput.ToLower().Contains("protein"));
-            Assert.True(sInput.ToLower().Contains("large"));
+            String sInput = oSession.OnMessage("1")[0];//option for exercise
+            Assert.True(sInput.Equals("Great! You'd like to exercise."));
         }
+
+
+
         [Fact]
-        public void TestChicken()
+        public void TestMeal()
         {
-            string sPath = DB.GetConnectionString();
             Session oSession = new Session("12345");
             oSession.OnMessage("hello");
-            oSession.OnMessage("large");
-            String sInput = oSession.OnMessage("chicken")[0];
-            Assert.True(sInput.ToLower().Contains("toppings"));
-            Assert.True(sInput.ToLower().Contains("large"));
-            Assert.True(sInput.ToLower().Contains("chicken"));
+            String sInput = oSession.OnMessage("2")[0];//option for meal
+            Assert.True(sInput.Equals("Awesome, I can absolutely help you with meal recommendations."));
         }
+        [Fact]
+        public void TestCalorie()
+        {
+            Session oSession = new Session("12345");
+            oSession.OnMessage("hello");
+            String sInput = oSession.OnMessage("3")[0];//option for calorie
+            Assert.True(sInput.Equals("Perfect choice, tracking calories is important in your fitness journey."));
+        }
+        [Fact]
+        public void TestBMI()
+        {
+            Session oSession = new Session("12345");
+            oSession.OnMessage("hello");
+            String sInput = oSession.OnMessage("4")[0];//option for bmi
+            Assert.True(sInput.Equals("Great! You wanna know your BMI!"));
+        }
+
+        [Fact]
+        public void TestContinue()
+        {
+            Session oSession = new Session("12345");
+            oSession.OnMessage("hello");
+             oSession.OnMessage("2"); //choose meal
+            String sInput = oSession.OnMessage("1200")[1]; // continue message
+            Assert.True(sInput.Equals("Anything else I can help you with?"));
+        }
+
+        [Fact]
+        public void TestEnd()
+        {
+            Session oSession = new Session("12345");
+            oSession.OnMessage("hello");
+            oSession.OnMessage("2"); //choose meal
+            oSession.OnMessage("1200"); // input for calorie
+            String sInput = oSession.OnMessage("5")[0]; // End message
+            Assert.True(sInput.Equals("Thank you for using Fit All Day! Have a nice day!"));
+        }
+
+        [Fact]
+        public void TestBMIFormula()
+        {
+            Session oSession = new Session("12345");
+            oSession.OnMessage("hello");
+            oSession.OnMessage("4"); //choose BMI
+            String sInput = oSession.OnMessage("161-55")[0]; // BMI calculator response
+            Assert.True(sInput.Contains("21.22"));
+        }
+
+      
     }
 }
